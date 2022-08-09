8bab5896404bfde2dcca3bd3dfc719606204171c
German Kulchinskiy <gdsgwwp@gmail.com>
2022-08-09 09:47:21 +0300

    Feature: modalWindows(sm desktops)

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 const editButton = document.querySelector(".profile__edit-button")
+const addButton = document.querySelector(".profile__add-button")
+
 const modalWindowBg = document.querySelector(".edit-from__bg")
 const modalWindowCloser = document.querySelector(".modal-window__closer")
-const addWindowBg = document.querySelector(".add-form__bg")
-const addFormCloser = document.querySelector(".add-form-closer")
-
-const addButton = document.querySelector(".profile__add-button")
 
 const newName = document.querySelector(".profile__name")
 const newJob = document.querySelector(".profile__job")
 
 const editFormName = document.querySelector(".modal-window__edited-name")
 const editFormJob = document.querySelector(".modal-window__edited-job")
-
 const editFormSubmit = document.querySelector(".edit-form-submit-button")
+
+const addWindowBg = document.querySelector(".add-form__bg")
+const addFormCloser = document.querySelector(".add-form-closer")
 const addFormSubmit = document.querySelector(".add-form-submit-button")
 
 const cardContainer = document.querySelector(".card-container")
@@ -101,7 +101,7 @@ photoCloser.addEventListener("click", () => {
     document.querySelector(".open-card__name").textContent = ''
 })
 
-addSong("Москва", "http://russiantourism.ru/netcat_files/2343_21053.jpg")
+addSong("Москва", "https://russiantourism.ru/netcat_files/2343_21053.jpg")
 addSong("Санкт-Петербург", "https://papik.pro/uploads/posts/2021-09/1631488111_12-papik-pro-p-peizazhi-peterburga-14.jpg")
 addSong("Алтай", "https://cdnn21.img.ria.ru/images/152155/85/1521558559_0:228:4368:2685_600x0_80_0_0_7096fe3ad8a519ec93377d5289428862.jpg")
 addSong("Карачаево-Черкессия", "https://s16.stc.all.kpcdn.net/russia/wp-content/uploads/2021/08/SHoaninskij_drevnehristianskij_hram__Karachaevo-CHerkesiya_Viki.jpg")
