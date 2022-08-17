899c151f8fd278bb501707568338598344aa8b63
German Kulchinskiy <gdsgwwp@gmail.com>
2022-08-17 19:58:37 +0300

    bugfix: normalize added

--- scripts/script.js
+++ scripts/script.js
@@ -36,6 +36,7 @@ modalWindowCloser.addEventListener("click", () => {
 
 addButton.addEventListener("click", (evt) => {
     evt.preventDefault()
+
     addWindowBg.classList.add("active")
     document.querySelector(".place-name").value = ''
     document.querySelector(".img-link").value = ''
@@ -49,13 +50,16 @@ addFormCloser.addEventListener("click", () => {
 
 editFormSubmit.addEventListener("click", (evt) => {
     evt.preventDefault()
+
     newName.textContent = editFormName.value
     newJob.textContent = editFormJob.value
+
     modalWindowBg.classList.remove("active")
+
     document.querySelector("body").style.overflow = "auto"
 })
 
-function addSong(placeValue, imgLink) {
+function addCard(placeValue, imgLink) {
     const cardTemplate = document.querySelector(".card-template").content
     const card = cardTemplate.querySelector(".card").cloneNode(true)
 
@@ -87,7 +91,7 @@ addFormSubmit.addEventListener("click", (evt) => {
     const place = document.querySelector(".place-name")
     const img = document.querySelector(".img-link")
 
-    addSong(place.value, img.value)
+    addCard(place.value, img.value)
 
     place.value = ''
     img.value = ''
@@ -101,9 +105,9 @@ photoCloser.addEventListener("click", () => {
     document.querySelector(".open-card__name").textContent = ''
 })
 
-addSong("Москва", "https://russiantourism.ru/netcat_files/2343_21053.jpg")
-addSong("Санкт-Петербург", "https://papik.pro/uploads/posts/2021-09/1631488111_12-papik-pro-p-peizazhi-peterburga-14.jpg")
-addSong("Алтай", "https://cdnn21.img.ria.ru/images/152155/85/1521558559_0:228:4368:2685_600x0_80_0_0_7096fe3ad8a519ec93377d5289428862.jpg")
-addSong("Карачаево-Черкессия", "https://s16.stc.all.kpcdn.net/russia/wp-content/uploads/2021/08/SHoaninskij_drevnehristianskij_hram__Karachaevo-CHerkesiya_Viki.jpg")
-addSong("гора Эльбрус", "https://wikiway.com/upload/iblock/902/Elbrus-na-zakate.jpg")
-addSong("Владивосток", "https://img.geliophoto.com/vl2017/00_vl2017.jpg")
+addCard("Москва", "https://russiantourism.ru/netcat_files/2343_21053.jpg")
+addCard("Санкт-Петербург", "https://papik.pro/uploads/posts/2021-09/1631488111_12-papik-pro-p-peizazhi-peterburga-14.jpg")
+addCard("Алтай", "https://cdnn21.img.ria.ru/images/152155/85/1521558559_0:228:4368:2685_600x0_80_0_0_7096fe3ad8a519ec93377d5289428862.jpg")
+addCard("Карачаево-Черкессия", "https://s16.stc.all.kpcdn.net/russia/wp-content/uploads/2021/08/SHoaninskij_drevnehristianskij_hram__Karachaevo-CHerkesiya_Viki.jpg")
+addCard("гора Эльбрус", "https://wikiway.com/upload/iblock/902/Elbrus-na-zakate.jpg")
+addCard("Владивосток", "https://img.geliophoto.com/vl2017/00_vl2017.jpg")
